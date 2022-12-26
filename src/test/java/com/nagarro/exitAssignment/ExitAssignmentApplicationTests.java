08813651aa223e7cf90b659ebc26aa4228654a56
Priya Gupta <87631126+Priyagpt353@users.noreply.github.com>
2022-12-26 23:19:56 +0530

    Add files via upload

---
+++ src/test/java/com/nagarro/exitAssignment/ExitAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.exitAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExitAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
